39c70da5049160bd20d08014ae2f7b4a9f6e84f0
Gerasimchick Dimitray <Gerasimchik_DS_25@student.grsu.by>
2025-10-14 09:51:43 +0300

    Update -A7.py
    removed unnecessary parentheses and type casts

--- software_engineering_labs/LAB-01/-A7.py
+++ software_engineering_labs/LAB-01/-A7.py
@@ -1,23 +1,23 @@
 first_position1 = int(input())
 second_position1 = int(input())
-summ_of_positions1 = int(first_position1 + second_position1)
+summ_of_positions1 = first_position1 + second_position1
 
 first_position2 = int(input())
 second_position2 = int(input())
-summ_of_positions2 = int(first_position2 + second_position2)
+summ_of_positions2 = first_position2 + second_position2
 
-if ((summ_of_positions1 % 2) == 0):
+if (summ_of_positions1 % 2) == 0:
     color1 = "White"
 else:
     color1 = "Black"
 
-if ((summ_of_positions2 % 2) == 0):
+if (summ_of_positions2 % 2) == 0:
     color2 = "White"
 else:
     color2 = "Black"
 
-if (color1 == color2):
-    if (color1 == "White"):
+if color1 == color2:
+    if color1 == "White":
         print("YES")
         print("White")
     else:
