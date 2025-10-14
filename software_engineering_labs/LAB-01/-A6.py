6fd779c52572217bc35049c20f677a754fe96535
Gerasimchick Dimitray <Gerasimchik_DS_25@student.grsu.by>
2025-10-14 08:39:24 +0300

    Update -A6.py
    fix seconds value issues

--- software_engineering_labs/LAB-01/-A6.py
+++ software_engineering_labs/LAB-01/-A6.py
@@ -1,5 +1,5 @@
 Input_Value = int(input())
-seconds = Input_Value % 3600
+seconds = Input_Value % 3600 % 60
 minutes = (Input_Value % 3600) // 60
 hours = Input_Value // 3600
 print('{:02}:{:02}:{:02}'.format(hours, minutes, seconds))
