41eab63529d99aaa93afbac28a1541f12ec93156
Gerasimchick Dimitray <Gerasimchik_DS_25@student.grsu.by>
2025-10-21 09:55:23 +0300

    add more pretty outputl A2.py

--- software_engineering_labs/LAB-02/-A2.py
+++ software_engineering_labs/LAB-02/-A2.py
@@ -3,33 +3,41 @@ import string
 password = input()
 upper_string = password.upper()
 lower_string = password.lower()
+list_of_errors = []
 
-Digit_flag = False
-Allowed_flag = False
+digit_flag = False
+allowed_flag = True
 special_character_flag = False
+is_first_digit = False
 
 allowed = string.ascii_uppercase + string.ascii_lowercase + string.digits + "+-#"
 special_characters = "+-#"
 
-Digit_flag = any(symbol.isdigit() for symbol in password)
-Allowed_flag = all(symbol in allowed for symbol in password)
+digit_flag = any(symbol.isdigit() for symbol in password)
+allowed_flag = all(symbol in allowed for symbol in password)
 for symbol in password:
     if symbol in set(special_characters):
         special_character_flag = True
 
-if len(password) != 8:
-    print("Длина пароля не равна 8")
-elif password == upper_string:
-    print("В пароле отсутствуют строчные буквы")
-elif password == lower_string:
-    print("В пароле отсутствуют заглавные буквы")
-elif Digit_flag == False:
-    print("В пароле отсутствуют цифры")
-elif Allowed_flag == False:
-    print("В пароле присутствуют непридусмотренные символы")
-elif special_character_flag == False:
-    print("В пароле отсутствуют специальные символы")
+if(password[0].isdigit()):
+    is_first_digit = True
+
+if len(password) < 8:
+    list_of_errors.append("Длина пароля меньше 8")
+if password == upper_string:
+    list_of_errors.append("В пароле отсутствуют строчные буквы")
+if password == lower_string:
+    list_of_errors.append("В пароле отсутствуют заглавные буквы")
+if digit_flag == False:
+    list_of_errors.append("В пароле отсутствуют цифры")
+if allowed_flag == False:
+    list_of_errors.append("В пароле присутствуют непридусмотренные символы")
+if special_character_flag == False:
+    list_of_errors.append("В пароле отсутсвуют специальные символы")
+if is_first_digit == True:
+    list_of_errors.append("В пароле первый символ не должен быть цифрой")
+
+if (len(list_of_errors) != 0):
+    print(list_of_errors)
 else:
-    print("Надежный пароль")
-
-
+    print("Пароль надежен")
