3c50e6e377e41e04c7174a2784ef68ba656e356c
Gerasimchick Dimitray <Gerasimchik_DS_25@student.grsu.by>
2025-10-27 18:07:43 +0300

    Update -A2.py
    Создан вывод прямоугольна и треугольника

--- software_engineering_labs/LAB-03/-A2.py
+++ software_engineering_labs/LAB-03/-A2.py
@@ -1,2 +1,23 @@
-n = int(input("Введите высоту прямоугольника"))
-m = int(input("Введите ширину прямоугольника"))
+while True:
+    try:
+        height_of_the_rectangle = int(input("Введите высоту прямоугольника: "))
+        width_of_the_rectangle = int(input("Введите ширину прямоугольника: "))
+        break
+    except ValueError:
+        print("Ширина и высота - целые числа!")
+
+for i in range(height_of_the_rectangle):
+    print(width_of_the_rectangle * '#')
+
+while True:
+    try:
+        height_of_the_triangle = int(input("Введите высоту треуголника: "))
+        break
+    except ValueError:
+        print("Высота - целые числа!")
+
+
+for i in range(height_of_the_triangle + 1):
+    print(i * '#')
+
+
