ddf7fb225d500f50f5114c6c46471d9945624972
Gerasimchick Dimitray <Gerasimchik_DS_25@student.grsu.by>
2025-11-16 16:58:24 +0300

    Fix output issues -A2.py

--- software_engineering_labs/LAB-04/-A2.py
+++ software_engineering_labs/LAB-04/-A2.py
@@ -5,6 +5,7 @@ for i in range(0, len(text)):
     if text[i] == '.' or text[i] == '!' or text[i] == '?':
         list_separatory_chars.append(i)
 
-for i in range(0, len(list_separatory_chars)):
-    print(text[list_separatory_chars[i]:list_separatory_chars[i + 1]])
-print(len(list_separatory_chars))
+print(text[0:list_separatory_chars[0]] + text[list_separatory_chars[0]])
+for i in range(0, len(list_separatory_chars) - 1):
+    print((text[(list_separatory_chars[i] + 1):list_separatory_chars[i + 1]] + text[list_separatory_chars[i + 1]]).strip())
+print("Колличество предложений: ", len(list_separatory_chars))
