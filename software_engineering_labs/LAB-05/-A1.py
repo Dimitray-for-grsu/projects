de1a5591f736fed00fb7d971b64d60c7bccb9a0c
Gerasimchick Dimitray <Gerasimchik_DS_25@student.grsu.by>
2025-12-08 21:43:29 +0300

    add load workout data from workouts.xml

--- software_engineering_labs/LAB-05/-A1.py
+++ software_engineering_labs/LAB-05/-A1.py
@@ -14,10 +14,34 @@ def load_users_data():
                 'age': int(user_elem.find('age').text),
                 'weight': int(user_elem.find('weight').text),
                 'fitness_level': user_elem.find('fitness_level').text,
-                'workouts': [] 
+                'workouts': []
             }
             users.append(user)
         return users
     except FileNotFoundError:
         print("Файл users.xml не найден")
+        return []
+
+
+def load_workouts_data():
+
+    try:
+        workouts_tree = ET.parse('workouts.xml')
+        workouts = []
+        for workout_elem in workouts_tree.getroot().findall('workout'):
+            workout = {
+                'workout_id': int(workout_elem.find('workout_id').text),
+                'user_id': int(workout_elem.find('user_id').text),
+                'date': workout_elem.find('date').text,
+                'type': workout_elem.find('type').text,
+                'duration': int(workout_elem.find('duration').text),
+                'distance': float(workout_elem.find('distance').text),
+                'calories': int(workout_elem.find('calories').text),
+                'avg_heart_rate': int(workout_elem.find('avg_heart_rate').text),
+                'intensity': workout_elem.find('intensity').text
+            }
+            workouts.append(workout)
+        return workouts
+    except FileNotFoundError:
+        print("Файл workouts.xml не найден")
         return []
